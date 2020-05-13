2a9653b0cbe569f84f1874c9fdedb5dd1bed289a
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-05-14 00:18:18 +0700

    update home page

---
+++ lib/screens/home.dart
@@ -0,0 +1,94 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  //Method
+
+  Widget showLogo() {
+    return Container(
+      width: 120.0,
+      height: 120.0,
+      child: Image.asset('images/logo.png'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      "App Shopping Mall",
+      style: TextStyle(
+          fontSize: 30.0,
+          color: Colors.red[900],
+          //fontWeight: FontWeight.bold,
+          // fontStyle: FontStyle.italic,
+          fontFamily: 'Indie Flower'),
+    );
+  }
+
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.blue.shade700,
+      child: Text(
+        'sign in',
+        style: TextStyle(
+          color: Colors.white,
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget signUpButton() {
+    return OutlineButton(
+      child: Text(
+        'sign up',
+        style: TextStyle(
+          color: Colors.red[900],
+        ),
+      ),
+      onPressed: () {},
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        signInButton(),
+        SizedBox(
+          width: 2.0,
+        ),
+        signUpButton()
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              showLogo(),
+              SizedBox(
+                height: 8.0,
+              ),
+              showAppName(),
+              SizedBox(
+                height: 8.0,
+              ),
+              showButton(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
