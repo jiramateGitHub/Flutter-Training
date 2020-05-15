9e4b921b2abed8ab238a6edc496abce20f30b7ae
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-05-15 21:15:01 +0700

    update upload to firebase_storage

--- lib/widget/add_list_product.dart
+++ lib/widget/add_list_product.dart
@@ -1,7 +1,10 @@
-import 'dart:math';
 import 'dart:io';
+import 'dart:math';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:appshoppingmall/screens/my_service.dart';
 
 class AddListProduct extends StatefulWidget {
   @override
@@ -11,8 +14,29 @@ class AddListProduct extends StatefulWidget {
 class _AddListProductState extends State<AddListProduct> {
   //Field
   File file;
+  String name, detail, urlPicture;
 
   //Method
+
+  Future<void> showAlert(String title, String message) async {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            FlatButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: Text('Ok'),
+            )
+          ],
+        );
+      },
+    );
+  }
 
   Widget uploadButton() {
     return Column(
@@ -22,7 +46,17 @@ class _AddListProductState extends State<AddListProduct> {
           width: MediaQuery.of(context).size.width,
           child: RaisedButton.icon(
             color: Colors.red,
-            onPressed: () {},
+            onPressed: () {
+              if (file == null) {
+                showAlert('Non Choose Picture', 'Please Choose Picture');
+              } else if (name == '' || detail == null) {
+                showAlert('Have Space', 'Please fill every Blank');
+              } else if (detail == '' || detail == null) {
+                showAlert('Have Space', 'Please fill every Blank');
+              } else {
+                uploadPictureToStore();
+              }
+            },
             icon: Icon(
               Icons.cloud_upload,
               color: Colors.white,
@@ -35,10 +69,46 @@ class _AddListProductState extends State<AddListProduct> {
     );
   }
 
+  Future<void> uploadPictureToStore() async {
+    Random random = Random();
+    int i = random.nextInt(10000);
+
+    FirebaseStorage firebaseStorage = FirebaseStorage.instance;
+    StorageReference storageReference =
+        firebaseStorage.ref().child('Product/product$i.jpg');
+    StorageUploadTask storageUploadTask = storageReference.putFile(file);
+
+    urlPicture =
+        await (await storageUploadTask.onComplete).ref.getDownloadURL();
+    print("urlPicture : $urlPicture");
+    insertValueToFireStore();
+  }
+
+  Future<void> insertValueToFireStore() async {
+    Firestore firestore = Firestore.instance;
+
+    Map<String, dynamic> map = Map();
+    map['name'] = name;
+    map['detail'] = detail;
+    map['path'] = urlPicture;
+
+    await firestore.collection('Product').document().setData(map).then((value) {
+      print('Insert Success');
+      MaterialPageRoute materialPageRoute = MaterialPageRoute(
+        builder: (BuildContext context) => MyService(),
+      );
+      Navigator.of(context).pushAndRemoveUntil(
+          materialPageRoute, (Route<dynamic> route) => false);
+    });
+  }
+
   Widget nameForm() {
     return Container(
       width: MediaQuery.of(context).size.width * 0.5,
       child: TextField(
+        onChanged: (String string) {
+          name = string.trim();
+        },
         decoration: InputDecoration(
           labelText: 'Name Product',
           helperText: 'Type Your Name of Product',
@@ -52,6 +122,9 @@ class _AddListProductState extends State<AddListProduct> {
     return Container(
       width: MediaQuery.of(context).size.width * 0.5,
       child: TextField(
+        onChanged: (String string) {
+          detail = string.trim();
+        },
         decoration: InputDecoration(
           labelText: 'Detail',
           helperText: 'Type Your Detail of Product',
@@ -112,7 +185,9 @@ class _AddListProductState extends State<AddListProduct> {
   Widget showImage() {
     return Container(
       padding: EdgeInsets.all(20.0),
-      child: file == null ? Image.asset('images/iconfinder_image.png') : Image.file(file),
+      child: file == null
+          ? Image.asset('images/iconfinder_image.png')
+          : Image.file(file),
       width: MediaQuery.of(context).size.width,
       height: MediaQuery.of(context).size.height * 0.3,
     );
