212ad6e44104400b03278c0e1595c8fdf10108ba
Jiramate Phuaphan <48597732+jiramateGitHub@users.noreply.github.com>
2020-05-15 20:19:08 +0700

    update image_picker

--- lib/widget/add_list_product.dart
+++ lib/widget/add_list_product.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class AddListProduct extends StatefulWidget {
   @override
@@ -6,10 +9,138 @@ class AddListProduct extends StatefulWidget {
 }
 
 class _AddListProductState extends State<AddListProduct> {
+  //Field
+  File file;
+
+  //Method
+
+  Widget uploadButton() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: <Widget>[
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: RaisedButton.icon(
+            color: Colors.red,
+            onPressed: () {},
+            icon: Icon(
+              Icons.cloud_upload,
+              color: Colors.white,
+            ),
+            label: Text('Upload'),
+            textColor: Colors.white,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget nameForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Name Product',
+          helperText: 'Type Your Name of Product',
+          icon: Icon(Icons.face),
+        ),
+      ),
+    );
+  }
+
+  Widget detailForm() {
+    return Container(
+      width: MediaQuery.of(context).size.width * 0.5,
+      child: TextField(
+        decoration: InputDecoration(
+          labelText: 'Detail',
+          helperText: 'Type Your Detail of Product',
+          icon: Icon(Icons.details),
+        ),
+      ),
+    );
+  }
+
+  Future<void> chooseImage(ImageSource imageSource) async {
+    try {
+      var object = await ImagePicker.pickImage(
+        source: imageSource,
+        maxWidth: 800.0,
+        maxHeight: 800.0,
+      );
+
+      setState(() {
+        file = object;
+      });
+    } catch (e) {}
+  }
+
+  Widget galleryButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_photo_alternate,
+        size: 36.0,
+      ),
+      onPressed: () {
+        chooseImage(ImageSource.gallery);
+      },
+    );
+  }
+
+  Widget cameraButton() {
+    return IconButton(
+      icon: Icon(
+        Icons.add_a_photo,
+        size: 36.0,
+      ),
+      onPressed: () {
+        chooseImage(ImageSource.camera);
+      },
+    );
+  }
+
+  Widget showButton() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceAround,
+      children: <Widget>[
+        cameraButton(),
+        galleryButton(),
+      ],
+    );
+  }
+
+  Widget showImage() {
+    return Container(
+      padding: EdgeInsets.all(20.0),
+      child: file == null ? Image.asset('images/iconfinder_image.png') : Image.file(file),
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height * 0.3,
+    );
+  }
+
+  Widget showContent() {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          showImage(),
+          showButton(),
+          nameForm(),
+          detailForm(),
+        ],
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      
+      child: Stack(
+        children: <Widget>[
+          showContent(),
+          uploadButton(),
+        ],
+      ),
     );
   }
 }
--- lib/widget/show_list_product.dart
+++ lib/widget/show_list_product.dart
@@ -1,3 +1,4 @@
+import 'package:appshoppingmall/models/product_model.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
@@ -8,6 +9,7 @@ class ShowListProduct extends StatefulWidget {
 
 class _ShowListProductState extends State<ShowListProduct> {
   // Field
+  List<ProductModel> productModels = List();
 
   // Method
   @override
@@ -27,14 +29,86 @@ class _ShowListProductState extends State<ShowListProduct> {
         print('snapshot = $snapshot');
         print('snapshot = ${snapshot.data['name']}');
 
+        ProductModel productModel = ProductModel.fromMap(snapshot.data);
+        setState(() {
+          productModels.add(productModel);
+        });
       }
     });
+  }
+
+  Widget showName(int index) {
+    return Row(
+      children: <Widget>[
+        Text(
+          productModels[index].name,
+          style: TextStyle(
+            fontSize: 24.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget showDetail(int index) {
+    String str = productModels[index].detail;
+    if (str.length > 100) {
+      str = str.substring(0, 90);
+      str = '$str ...';
+    }
+    return Text(
+      str,
+      style: TextStyle(
+        fontSize: 14.0,
+        fontStyle: FontStyle.italic,
+      ),
+    );
+  }
+
+  Widget showText(int index) {
+    return Container(
+        width: MediaQuery.of(context).size.width * 0.5,
+        height: MediaQuery.of(context).size.width * 0.5,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[
+            showName(index),
+            showDetail(index),
+          ],
+        ));
+  }
+
+  Widget showImage(int index) {
+    return Container(
+        padding: EdgeInsets.all(20.0),
+        width: MediaQuery.of(context).size.width * 0.5,
+        height: MediaQuery.of(context).size.width * 0.5,
+        child: Container(
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10.0),
+              image: DecorationImage(
+                image: NetworkImage(productModels[index].path),
+                fit: BoxFit.cover,
+              )),
+        ));
+  }
+
+  Widget showListView(int index) {
+    return Row(
+      children: <Widget>[showImage(index), showText(index)],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('This is ShowListProuct'),
+      child: ListView.builder(
+        itemCount: productModels.length,
+        itemBuilder: (BuildContext buildContext, int index) {
+          return showListView(index);
+        },
+      ),
     );
   }
 }
